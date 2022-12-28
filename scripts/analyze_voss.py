c5578a1077bcdf4d130e0c0082bd669f936e84be
Kenneth J. Grace <kennethjeffersongrace@gmail.com>
2022-12-28 13:17:41 -0800

    path fix

--- scripts/analyze_voss.py
+++ scripts/analyze_voss.py
@@ -6,20 +6,27 @@ ISIS Adjacencies, LLDP Neighbors, MAC Learning, and Port State. This script will
 that information as a table in CSV format for use in Microsoft Excel. Exceptions are not
 handled to provide a full stack trace in the event an exception does occur.
 """
-
-import json
 import sys
 import os
+import json
 import argparse
 from pathlib import Path
 
-from voss.voss import VOSS
+# Set the path to the parent directory if this script is run from the scripts' directory
+# else set the path to the current directory
+path = os.getcwd() if os.getcwd() != os.path.dirname(os.path.realpath(__file__)) else '..'
+try:
+    sys.path.index(path)
+except ValueError:
+    sys.path.append(path)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="A Network State Analysis Tool")
     parser.add_argument("filename", type=Path, help="a filepath to a VOSS tech file")
     parser.add_argument("--output", "-o", type=Path, default=os.getcwd(), help="a filepath for the analysis output")
     args = parser.parse_args(sys.argv[1:])
+    from voss import VOSS
     voss = VOSS.load(args.filename)
     # Search through the tech file for the commands we're looking for.
     # Pump the corresponding output through the bound parsing function.
